a7dbfc924e1b03d71b3f14e780d8044330d13174
msterbi <50196622+msterbi@users.noreply.github.com>
2024-01-21 18:08:50 +0100

    Add files via upload
    Added support for games which are syncing config data to Steam Cloud

--- docksettings.sh
+++ docksettings.sh
@@ -3,12 +3,14 @@
 ## Changelog
 ## 15-01-2024 v0.1 Initial release
 ## 19-01-2024 v0.2 Display help; replace positional parameters with options; allow prefix shortcut to steamapps location
+## 20-01-2024 v0.3 Added support for games which are syncing config data to Steam Cloud
 
 
 ## Define basic variables
 
 ROOTDIR=/home/deck/Documents
 DATE=$(date '+%d-%m-%Y %H:%M:%S')
+CLOUDSLEEP=10
 
 
 ## Display help message
@@ -19,19 +21,22 @@ help()
     echo "-h    Print this help"
     echo "-n    Name of game"
     echo "-f    Location of config file"
+    echo "-c    Name of game's executable (optional; for games which are syncing config data to Steam Cloud)"
     echo ""
     echo "Examples:"
     echo "./docksettings.sh -n \"Resident Evil 2\" -f \"/home/deck/.steam/steam/steamapps/common/RESIDENT EVIL 2  BIOHAZARD RE2/re2_config.ini\""
     echo "./docksettings.sh -n \"Resident Evil 2\" -f \"NVME/common/RESIDENT EVIL 2  BIOHAZARD RE2/re2_config.ini\""
     echo "./docksettings.sh -n \"Resident Evil 2\" -f \"SD/common/RESIDENT EVIL 2  BIOHAZARD RE2/re2_config.ini\""
+    echo "./docksettings.sh -n \"NieR Automata\" -f \"NVME/compatdata/524220/pfx/drive_c/users/steamuser/Documents/My Games/NieR_Automata/SystemData.dat\" -c \"NieRAutomata.exe\""
 }
 
 
 ## Get the options
 
-while getopts "hcn:f:" option; do
+while getopts "hc:n:f:" option; do
     case $option in
         h) help; exit;;
+        c) GAMEEXE=$OPTARG;;
         n) NAME=$OPTARG;;
         f) FILE=$OPTARG;;
         \?) echo "$DATE Invalid options have been used." >> "$ROOTDIR/docksettings_error"; exit;;
@@ -90,7 +95,7 @@ else
 fi
 
 
-## Create initial backup and two profiles of savefile
+## Create initial backup and two profiles of configfile
 
 if [ -f "$ROOTDIR/docksettings/$NAME/backup_$CONFIG" ]; then
     echo "$DATE INFO: Initial backup and profiles already exist." >> "$LOGFILE"
@@ -112,16 +117,18 @@ fi
 
 ## Read last state value
 
-LASTSTATE=$(cat "$ROOTDIR/docksettings/$NAME/laststate")
+if [ "$GAMEEXE" = "" ]; then
+    LASTSTATE=$(cat "$ROOTDIR/docksettings/$NAME/laststate")
 
-if [ "$LASTSTATE" = "0" ]; then
-    echo "$DATE INFO: Last state was 'undocked'." >> "$LOGFILE"
-elif [ "$LASTSTATE" = "1" ]; then
-    echo "$DATE INFO: Last state 'docked'." >> "$LOGFILE"
-else
-    echo "$DATE ERROR: Last state was 'unknown'. Exiting." >> "$LOGFILE"
-    echo "------------------------------------" >> "$LOGFILE"
-    exit 1
+    if [ "$LASTSTATE" = "0" ]; then
+        echo "$DATE INFO: Last state was 'undocked'." >> "$LOGFILE"
+    elif [ "$LASTSTATE" = "1" ]; then
+        echo "$DATE INFO: Last state 'docked'." >> "$LOGFILE"
+    else
+        echo "$DATE ERROR: Last state was 'unknown'. Exiting." >> "$LOGFILE"
+        echo "------------------------------------" >> "$LOGFILE"
+        exit 1
+    fi
 fi
 
 
@@ -143,6 +150,54 @@ else
     DOCK=1
     echo 1 > "$ROOTDIR/docksettings/$NAME/laststate"
     echo "$DATE INFO: Current state is 'docked'." >> "$LOGFILE"
+fi
+
+
+## Implement logic for cloud-based config files
+
+if [ "$GAMEEXE" != "" ]; then
+    echo "$DATE INFO: Starting cloud-based config file syncing sequence." >> "$LOGFILE"
+    if [ "$DOCK" = "0" ]; then
+        cp -p "$ROOTDIR/docksettings/$NAME/deck/deck_$CONFIG" "$FILE"
+        echo "$DATE CHANGE: Copying cloud-based config file $ROOTDIR/docksettings/$NAME/deck/deck_$CONFIG to $FILE" >> "$LOGFILE"
+    elif [ "$DOCK" = "1" ]; then
+        cp -p "$ROOTDIR/docksettings/$NAME/dock/dock_$CONFIG" "$FILE"
+        echo "$DATE CHANGE: Copying cloud-based config file $ROOTDIR/docksettings/$NAME/dock/dock_$CONFIG to $FILE" >> "$LOGFILE"
+    fi
+
+    while true
+    do
+        ps -efa | grep $GAMEEXE | grep -v $0 | grep -v grep >/dev/null 2>&1
+        if [ $? -eq 1 ]; then
+            SHA1=$(sha1sum "$FILE" | cut -d " " -f1)
+            echo "$(date '+%d-%m-%Y %H:%M:%S') INFO: Checksum of $FILE is $SHA1" >> "$LOGFILE"
+            if [ "$DOCK" = "0" ]; then
+                SHA2=$(sha1sum "$ROOTDIR/docksettings/$NAME/deck/deck_$CONFIG" | cut -d " " -f1)
+                echo "$(date '+%d-%m-%Y %H:%M:%S') INFO: Checksum of deck_$CONFIG is $SHA2" >> "$LOGFILE"
+                if [ "$SHA1" == "$SHA2" ]; then
+                    echo "$(date '+%d-%m-%Y %H:%M:%S') INFO: Checksum matched. Nothing to do." >> "$LOGFILE"
+                else
+                    cp -p "$FILE" "$ROOTDIR/docksettings/$NAME/deck/deck_$CONFIG"
+                    echo "$(date '+%d-%m-%Y %H:%M:%S') CHANGE: Config file has been updated. Syncing." >> "$LOGFILE"
+                fi
+            elif [ "$DOCK" = "1" ]; then
+                SHA2=$(sha1sum "$ROOTDIR/docksettings/$NAME/dock/dock_$CONFIG" | cut -d " " -f1)
+                echo "$(date '+%d-%m-%Y %H:%M:%S') INFO: Checksum of dock_$CONFIG is $SHA2" >> "$LOGFILE"
+                if [ "$SHA1" == "$SHA2" ]; then
+                    echo "$(date '+%d-%m-%Y %H:%M:%S') INFO: Checksum matched. Nothing to do." >> "$LOGFILE"
+                else
+                    cp -p "$FILE" "$ROOTDIR/docksettings/$NAME/dock/dock_$CONFIG"
+                    echo "$(date '+%d-%m-%Y %H:%M:%S') CHANGE: Config file has been updated. Syncing." >> "$LOGFILE"
+                fi
+            fi
+            echo "$(date '+%d-%m-%Y %H:%M:%S') CHANGE: Process $GAMEEXE no longer running. Exiting." >> "$LOGFILE"
+            echo "------------------------------------" >> "$LOGFILE"
+            exit 0
+        else
+            echo "$(date '+%d-%m-%Y %H:%M:%S') INFO: Process $GAMEEXE still running. Sleeping." >> "$LOGFILE"
+            sleep $CLOUDSLEEP
+        fi
+    done
 fi
 
 
